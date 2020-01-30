5e835dcbbda12f0cdccf26cd7227f303c8908ff2
Nitin Tharwani <tharwaninitin182@gmail.com>
2020-01-30 20:34:07 +0530

    updated github_release.sh

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -6,4 +6,5 @@ echo $GITHUB_TOKEN
 
 # https://github.com/tcnksm/ghr
 # Uploads jar to Github releases page
-ghr v0.6.0 etljobs/target/scala-2.11/etljobs_2.11-0.6.0.jar
+ghr v0.7.0 etljobs/target/scala-2.11/etljobs_2.11-0.7.0.jar
+ghr v0.7.0 etljobs/target/scala-2.12/etljobs_2.12-0.7.0.jar

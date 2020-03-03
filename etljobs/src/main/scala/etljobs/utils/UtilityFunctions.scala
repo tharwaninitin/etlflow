6864b62e971cc7164cbf6d3ed9dd441f2885912b
Nitin Tharwani <tharwaninitin182@gmail.com>
2020-03-03 19:55:46 +0530

    Made etljob name typed in multiple places

--- etljobs/src/main/scala/etljobs/utils/UtilityFunctions.scala
+++ etljobs/src/main/scala/etljobs/utils/UtilityFunctions.scala
@@ -5,6 +5,8 @@ import org.apache.log4j.Logger
 import scala.reflect.ClassTag
 import scala.util.{Failure, Success, Try}
 import scala.reflect.runtime.universe.TypeTag
+import scala.reflect.runtime.{universe => ru}
+import scala.util.control.Exception._
 
 trait UtilityFunctions {
   lazy val uf_logger: Logger = Logger.getLogger(getClass.getName)
@@ -18,19 +20,25 @@ trait UtilityFunctions {
     }.toMap
   }
   def printEtlJobs[T: TypeTag]: Unit = {
-    import scala.reflect.runtime.{universe => ru}
     val tpe = ru.typeOf[T]
     val clazz = tpe.typeSymbol.asClass
     val allJobNames = clazz.knownDirectSubclasses
     allJobNames.foreach(uf_logger.info(_))
   }
-  //  val job_name = getEtlJobName(job_properties("job_name"))
-  //  def getEtlJobName(job_name: String): Option[MyEtlJobName] = {
-  //    val job = MyEtlJobName.fromString(job_name)
-  //    job
-  //  }
-  //  val my_global_properties = getGlobalProperties[MyGlobalProperties]("loaddata.properties")
-  //  val my_global_properties = getGlobalProperties("loaddata.properties")(new MyGlobalProperties(_))
+  def getEtlJobName[T: TypeTag](job_name: String): T = {
+    val fullClassName = "etljobs.schema.EtlJobList$" + job_name + "$"
+
+    try {
+      val classVal = Class.forName(fullClassName)
+      val constructor = classVal.getConstructor()
+      constructor.newInstance().asInstanceOf[T]
+    }
+    catch {
+      case e: ClassNotFoundException =>
+        uf_logger.error(s"Tried creating object with path $fullClassName, but failed with error")
+        throw e
+    }
+  }
   def getGlobalPropertiesUsingReflection[T <: GlobalProperties](path: String = "loaddata.properties")(implicit tag: ClassTag[T]): Option[T] = {
     Try {
       tag.runtimeClass.getConstructor(classOf[String]).newInstance(path).asInstanceOf[T]
--- etljobs/src/test/scala/etljobs/LoadData.scala
+++ etljobs/src/test/scala/etljobs/LoadData.scala
@@ -2,7 +2,6 @@ package etljobs
 
 import etljobs.schema.EtlJobList.{EtlJob1PARQUETtoORCtoBQLocalWith2StepsWithSlack, EtlJob2CSVtoPARQUETtoBQLocalWith3Steps, EtlJob3CSVtoPARQUETtoBQGcsWith2Steps, MyEtlJobName}
 import etljobs.schema.EtlJobProps.{EtlJob1Props, EtlJob23Props}
-
 import scala.util.Try
 
 object LoadData extends EtlJobApp[MyEtlJobName] {
@@ -11,8 +10,8 @@ object LoadData extends EtlJobApp[MyEtlJobName] {
   val send_notification = true
   val notification_level = "debug"
 
-  def toEtlJob(job_name: String, job_properties: Map[String, String]): EtlJob = {
-    val job1Props = EtlJob1Props(
+  def toEtlJob(job_name: MyEtlJobName, job_properties: Map[String, String]): EtlJob = {
+    lazy val job1Props = EtlJob1Props(
       job_run_id = java.util.UUID.randomUUID.toString,
       job_name = EtlJob1PARQUETtoORCtoBQLocalWith2StepsWithSlack,
       ratings_input_path = s"$canonical_path/etljobs/src/test/resources/input/movies/ratings_parquet/*",
@@ -23,11 +22,11 @@ object LoadData extends EtlJobApp[MyEtlJobName] {
     )
 
     job_name match {
-      case "EtlJob1PARQUETtoORCtoBQLocalWith2StepsWithSlack" =>
+      case EtlJob1PARQUETtoORCtoBQLocalWith2StepsWithSlack =>
         new etljob1.EtlJobDefinition(
           job_properties = job1Props
         )
-      case "EtlJob2CSVtoPARQUETtoBQLocalWith3Steps" =>
+      case EtlJob2CSVtoPARQUETtoBQLocalWith3Steps =>
         new etljob2.EtlJobDefinition(
           job_properties = EtlJob23Props(
             job_run_id = java.util.UUID.randomUUID.toString,
@@ -38,7 +37,7 @@ object LoadData extends EtlJobApp[MyEtlJobName] {
             ratings_output_table_name = "ratings_par"
           )
         )
-      case "EtlJob3CSVtoPARQUETtoBQGcsWith2Steps" =>
+      case EtlJob3CSVtoPARQUETtoBQGcsWith2Steps =>
         new etljob3.EtlJobDefinition(
           job_properties = EtlJob23Props(
             job_run_id = java.util.UUID.randomUUID.toString,

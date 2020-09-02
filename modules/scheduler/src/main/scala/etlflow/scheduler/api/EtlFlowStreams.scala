90038c867ba965b5e57cd05be4c0d65b7abc678d
Swapnil S <50050327+Iamswapnil619@users.noreply.github.com>
2020-09-02 23:09:42 +0530

    Added support for circe parser, added test cases, Added error handling in stream
    * Added support for circe parser and test cases
    * Removed unwanted dependencies
    * Added error handling in stream and removed html page for jobListError

--- modules/scheduler/src/main/scala/etlflow/scheduler/api/EtlFlowStreams.scala
+++ modules/scheduler/src/main/scala/etlflow/scheduler/api/EtlFlowStreams.scala
@@ -2,15 +2,18 @@ package etlflow.scheduler.api
 
 import cats.effect.{ContextShift, Sync, Timer}
 import cats.{Applicative, Functor}
+import etlflow.scheduler.util.CacheHelper
 import fs2.{Pipe, Stream}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.websocket.WebSocketBuilder
 import org.http4s.websocket.WebSocketFrame
 import org.slf4j.{Logger, LoggerFactory}
+import scalacache.Cache
+
 import scala.concurrent.duration._
 
-class EtlFlowStreams[F[_]: Sync: ContextShift: Timer] extends Http4sDsl[F] {
+class EtlFlowStreams[F[_]: Sync: ContextShift: Timer](cache: Cache[String]) extends Http4sDsl[F] {
   lazy val logger: Logger = LoggerFactory.getLogger(getClass.getName)
   val mb: Int = 1024*1024
   val runtime: Runtime = Runtime.getRuntime
@@ -35,8 +38,13 @@ class EtlFlowStreams[F[_]: Sync: ContextShift: Timer] extends Http4sDsl[F] {
 
   lazy val streamRoutes: HttpRoutes[F] =
     HttpRoutes.of[F] {
-      case GET -> Root =>
-        val toClient: Stream[F, WebSocketFrame] = stream.map(s => WebSocketFrame.Text(s.toString))
+      case GET -> Root / token =>
+        val toClient: Stream[F, WebSocketFrame] =
+          if(CacheHelper.getKey(cache,token).getOrElse("NA") == token){
+            stream.map(s => WebSocketFrame.Text(s.toString))
+          } else {
+            Stream.empty
+        }
         val fromClient: Pipe[F, WebSocketFrame, Unit] = _.as(())
         WebSocketBuilder[F].build(toClient, fromClient, onClose = Applicative[F].pure(logger.info("Closed Web socket")))
     }

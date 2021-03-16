112ef0962328dfdc5aa5adc59088285e375b0139
Nitin Tharwani <tharwaninitin182@gmail.com>
2021-03-16 13:40:42 +0530

    BUG FIX - Updated GetCredentialStep

--- modules/core/src/main/scala/etlflow/etlsteps/GetCredentialStep.scala
+++ modules/core/src/main/scala/etlflow/etlsteps/GetCredentialStep.scala
@@ -4,11 +4,8 @@ import etlflow.jdbc.DbManager
 import etlflow.utils.Configuration
 import zio.Task
 
-case class GetCredentialStep[T : Manifest](
-                                       val name: String,
-                                       credential_name: String
-                                     ) extends EtlStep[Unit,T] with DbManager with Configuration {
+case class GetCredentialStep[T : Manifest](name: String, credential_name: String) extends EtlStep[Unit,T] with DbManager with Configuration {
   override def process(input_state: => Unit): Task[T] = {
-    getDbCredentials[T](name,config.dbLog,scala.concurrent.ExecutionContext.Implicits.global)
+    getDbCredentials[T](credential_name,config.dbLog,scala.concurrent.ExecutionContext.Implicits.global)
   }
 }

180997e0afef78869250326192244a2aa944f52f
Sonawane <sonawanes@BM6208.local>
2021-05-25 00:36:58 +0530

    Updated server test suites

--- modules/server/src/test/scala/etlflow/jdbc/DoobieTestSuite.scala
+++ modules/server/src/test/scala/etlflow/jdbc/DoobieTestSuite.scala
@@ -14,7 +14,16 @@ class DoobieTestSuite extends funsuite.AnyFunSuite with matchers.should.Matchers
   //implicit val dbLogger: log.LogHandler = DoobieQueryLogger()
   val transactor: doobie.Transactor[IO] = Transactor.fromDriverManager[IO](credentials.driver, credentials.url, credentials.user, credentials.password)
 
-  val creds: CredentialDB = CredentialDB("test", "jdbc", JsonString("{}"))
+  val cred_value =
+    """{
+        "url":"jdbc:postgresql://localhost:5432/etlflow",
+        "user": "localhost",
+        "driver": "org.postgresql.Driver",
+        "password": "localhost"
+       }"""
+      .stripMargin
+
+  val creds: CredentialDB = CredentialDB("test", "jdbc", JsonString(cred_value))
   val jobs = List(EtlJob("Job6", Map.empty),EtlJob("Job5", Map.empty))
   val jobsDB = jobs.map{x =>
     JobDB(x.name, x.props.getOrElse("job_schedule",""), is_active = true)
--- modules/server/src/test/scala/etlflow/webserver/GraphqlTestSuite.scala
+++ modules/server/src/test/scala/etlflow/webserver/GraphqlTestSuite.scala
@@ -28,7 +28,7 @@ object GraphqlTestSuite extends DefaultRunnableSpec with ServerSuiteHelper with 
           gqlResponse <- etlFlowInterpreter.flatMap(_.execute(query))
           _           = logger.info(gqlResponse.toString)
         } yield gqlResponse.data.toString
-        assertM(result)(equalTo("""{"jobs":[{"name":"Job1","is_active":true},{"name":"Job2","is_active":false}]}""")
+        assertM(result)(equalTo("""{"jobs":[{"name":"Job1","is_active":true},{"name":"Job2","is_active":false},{"name":"Job5","is_active":true}]}""")
         )
       },
       testM("Test query jobruns end point") {

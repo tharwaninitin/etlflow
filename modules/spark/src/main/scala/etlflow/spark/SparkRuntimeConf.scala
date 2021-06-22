7db8653ba479f2b2cb6bf83dea086d856bbf651f
Swapnil S <50050327+Iamswapnil619@users.noreply.github.com>
2021-06-23 01:11:50 +0530

    Downgraded json jackson version and updated spark configuration. (#120)

--- modules/spark/src/main/scala/etlflow/spark/SparkRuntimeConf.scala
+++ modules/spark/src/main/scala/etlflow/spark/SparkRuntimeConf.scala
@@ -5,12 +5,12 @@ import org.apache.spark.sql.SparkSession
 object SparkRuntimeConf {
   def apply(spark: SparkSession):  Map[String,String] = {
     val runtimeConf = spark.sparkContext.getConf
-    Map("executor_memory"-> runtimeConf.get("spark.executor.memory")
-      , "executor_cores"-> runtimeConf.get("spark.executor.cores")
-      , "executor_instances" -> runtimeConf.get("spark.executor.instances")
-      , "driver_memory" -> runtimeConf.get("spark.driver.memory")
-      , "yarn_memory" -> runtimeConf.get("spark.yarn.am.memory")
-      , "parallelism" -> runtimeConf.get("spark.default.parallelism")
+    Map("executor_memory"-> runtimeConf.getOption("spark.executor.memory").getOrElse("NA")
+      , "executor_cores"-> runtimeConf.getOption("spark.executor.cores").getOrElse("NA")
+      , "executor_instances" -> runtimeConf.getOption("spark.executor.instances").getOrElse("NA")
+      , "driver_memory" -> runtimeConf.getOption("spark.driver.memory").getOrElse("NA")
+      , "yarn_memory" -> runtimeConf.getOption("spark.yarn.am.memory").getOrElse("NA")
+      , "parallelism" -> runtimeConf.getOption("spark.default.parallelism").getOrElse("NA")
     )
   }
 }

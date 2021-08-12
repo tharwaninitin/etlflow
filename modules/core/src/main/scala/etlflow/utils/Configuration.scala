75ab5290f8dfa0510e8f43f753a60d2956d5c029
Swapnil S <50050327+Iamswapnil619@users.noreply.github.com>
2021-08-12 22:31:57 +0530

    Refactored zio-config to remove magnolia dependancy (#144)

--- modules/core/src/main/scala/etlflow/utils/Configuration.scala
+++ modules/core/src/main/scala/etlflow/utils/Configuration.scala
@@ -1,13 +1,32 @@
 package etlflow.utils
 
 import etlflow.schema.Config
-import zio.{ZIO, IO}
-import zio.config.magnolia.DeriveConfigDescriptor.descriptor
+import zio.{IO, ZIO}
 import zio.config.typesafe.TypesafeConfigSource
-import zio.config.{ReadError, read}
+import zio.config._
+import ConfigDescriptor._
+import etlflow.schema.Credential.JDBC
+import etlflow.schema._
 
 object Configuration {
+
+  val db: ConfigDescriptor[JDBC] = (string("url") |@| string("user") |@| string("password") |@| string("driver"))(JDBC.apply, JDBC.unapply)
+  val dataprocSpark = (string("mainclass") |@| list("deplibs")(string))(DataprocSpark.apply, DataprocSpark.unapply)
+  val slack = (string("url") |@| string("env") |@| string("host"))(Slack.apply, Slack.unapply)
+  val webServer = (string("ip_address").optional |@| int("port").optional |@| string("secretKey").optional
+    |@| set("allowedOrigins")(string).optional )(WebServer.apply, WebServer.unapply)
+
+
+  val applicationConf =
+    (nested("db")(db) |@|
+      string("timezone").optional |@|
+      nested("slack")(slack).optional |@|
+      nested("dataproc")(dataprocSpark).optional |@|
+      list("token")(string).optional |@|
+      nested("webserver")(webServer).optional
+      )(Config.apply, Config.unapply)
+
   lazy val config: IO[ReadError[String], Config] =
     TypesafeConfigSource.fromDefaultLoader
-      .flatMap(source => ZIO.fromEither(read(descriptor[Config] from source)))
+      .flatMap(source => ZIO.fromEither(read(applicationConf from source)))
 }

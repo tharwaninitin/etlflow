ef3a34b820ac11dd79e3c65be43cf21e13ef9909
Nitin Tharwani <tharwaninitin182@gmail.com>
2022-12-05 10:27:42 +0530

    Updated K8S module and GCP dependency

--- modules/gcp/src/main/scala/etlflow/task/GCSCopyTask.scala
+++ modules/gcp/src/main/scala/etlflow/task/GCSCopyTask.scala
@@ -12,9 +12,9 @@ case class GCSCopyTask(
     output: Location,
     parallelism: Int,
     overwrite: Boolean = true
-) extends EtlTask[GCS, Unit] {
+) extends EtlTask[GCS, Long] {
 
-  override protected def process: RIO[GCS, Unit] = {
+  override protected def process: RIO[GCS, Long] = {
     val program = (input, output) match {
       case (src @ Location.GCS(_, _), tgt @ Location.GCS(_, _)) =>
         GCS.copyObjectsGCStoGCS(
@@ -32,12 +32,12 @@ case class GCSCopyTask(
         ZIO.attempt(throw new RuntimeException(s"Copying data between source $src to target $tgt is not implemented yet"))
     }
     val runnable = for {
-      _ <- ZIO.succeed(logger.info("#" * 100))
-      _ <- ZIO.succeed(logger.info(s"Source Filesystem $input"))
-      _ <- ZIO.succeed(logger.info(s"Target Filesystem $output"))
-      _ <- program.tapError(ex => ZIO.succeed(logger.error(ex.getMessage)))
-      _ <- ZIO.succeed(logger.info(s"Successfully copied objects" + "\n" + "#" * 100))
-    } yield ()
+      _     <- ZIO.succeed(logger.info("#" * 100))
+      _     <- ZIO.succeed(logger.info(s"Source Filesystem $input"))
+      _     <- ZIO.succeed(logger.info(s"Target Filesystem $output"))
+      count <- program.tapError(ex => ZIO.succeed(logger.error(ex.getMessage)))
+      _     <- ZIO.succeed(logger.info(s"Successfully copied objects" + "\n" + "#" * 100))
+    } yield count
     runnable
   }
 
--- modules/k8s/src/test/scala/etlflow/task/CreateKubeJobTestSuite.scala
+++ modules/k8s/src/test/scala/etlflow/task/CreateKubeJobTestSuite.scala
@@ -2,7 +2,7 @@ package etlflow.task
 
 import com.coralogix.zio.k8s.client.batch.v1.jobs.Jobs
 import etlflow.audit.Audit
-import etlflow.k8s.K8S
+import etlflow.k8s.{K8S, Secret}
 import zio.ZIO
 import zio.test.Assertion.equalTo
 import zio.test._
@@ -13,7 +13,7 @@ object CreateKubeJobTestSuite {
       name = "KubeJobTaskExample",
       image = "busybox:1.28",
       command = Some(Vector("/bin/sh", "-c", "date; echo Hello from the Kubernetes cluster")),
-      secret = Some("secret")
+      secret = Some(Secret("secret", "/etc/secret"))
     ).execute
     assertZIO(task.foldZIO(ex => ZIO.fail(ex.getMessage), _ => ZIO.succeed("ok")))(equalTo("ok"))
   }

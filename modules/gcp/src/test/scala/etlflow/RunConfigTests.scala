9d8c3bcdaa140e26d53ccdb1c95a4b6a8f2b2b88
Nitin Tharwani <tharwaninitin182@gmail.com>
2022-12-31 01:22:57 +0530

    Update RunConfigTests.scala

--- modules/gcp/src/test/scala/etlflow/RunConfigTests.scala
+++ modules/gcp/src/test/scala/etlflow/RunConfigTests.scala
@@ -1,9 +1,14 @@
 package etlflow
 
 import etlflow.config._
+import etlflow.log.ApplicationLogger
+import zio.ULayer
 import zio.test._
 
-object RunConfigTests extends ZIOSpecDefault {
+object RunConfigTests extends ZIOSpecDefault with ApplicationLogger {
+
+  override val bootstrap: ULayer[TestEnvironment] = testEnvironment ++ zioSlf4jLogger
+
   override def spec: Spec[TestEnvironment, Any] = suite("GCP Config Checks")(
     DPSparkJobTaskConfig.spec
   ) @@ TestAspect.sequential

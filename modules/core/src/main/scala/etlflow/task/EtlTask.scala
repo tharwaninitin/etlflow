9ee447087e8f7e3a9e7e2ca8f28639ced617fb3b
Nitin Tharwani <tharwaninitin182@gmail.com>
2023-01-14 23:54:48 +0530

    PipelineTestCase update

--- modules/core/src/main/scala/etlflow/task/EtlTask.scala
+++ modules/core/src/main/scala/etlflow/task/EtlTask.scala
@@ -25,26 +25,26 @@ trait EtlTask[R, OP] extends ApplicationLogger {
 
   def map[B](f: OP => B): EtlTask[R, B] = EtlTask.map(this, f)
 
-  def flatMap[R1 <: Any, OP1](f: OP => EtlTask[R1, OP1]): EtlTask[R with R1, OP1] = EtlTask.flatMap[R, OP, R1, OP1](this, f)
+  def flatMap[R1, OP1](fn: OP => EtlTask[R1, OP1]): EtlTask[R with R1, OP1] = EtlTask.flatMap[R, OP, R1, OP1](this, fn)
 
-  def *>[R1 <: Any, OP1](that: EtlTask[R1, OP1]): EtlTask[R with R1, OP1] = EtlTask.flatMap[R, OP, R1, OP1](this, _ => that)
+  def *>[R1, OP1](that: EtlTask[R1, OP1]): EtlTask[R with R1, OP1] = EtlTask.flatMap[R, OP, R1, OP1](this, _ => that)
 }
 
-@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements", "org.wartremover.warts.AsInstanceOf"))
 object EtlTask {
-  val RT: AtomicReference[Set[String]] = new AtomicReference[Set[String]](Set.empty[String])
+  val taskSet: AtomicReference[Set[String]] = new AtomicReference[Set[String]](Set.empty[String])
 
-  def flatMap[R1, OP1, R2 <: Any, OP2](previousTask: EtlTask[R1, OP1], f: OP1 => EtlTask[R2, OP2]): EtlTask[R1 with R2, OP2] = {
-    RT.updateAndGet(_ + previousTask.name)
+  def flatMap[R1, OP1, R2, OP2](previousTask: EtlTask[R1, OP1], fn: OP1 => EtlTask[R2, OP2]): EtlTask[R1 with R2, OP2] = {
+    taskSet.updateAndGet(_ + previousTask.name)
     new EtlTask[R1 with R2, OP2] {
-      override protected def process: RIO[R1 with R2, OP2] = previousTask.process.flatMap(op => f(op).process)
+      override protected def process: RIO[R1 with R2, OP2] = previousTask.process.flatMap(op => fn(op).process)
       override val name: String                            = "Pipeline"
     }
   }
 
-  def map[R, A, OP](previousTask: EtlTask[R, A], f: A => OP): EtlTask[R, OP] =
+  def map[R, A, OP](previousTask: EtlTask[R, A], fn: A => OP): EtlTask[R, OP] =
     new EtlTask[R, OP] {
-      override protected def process: RIO[R, OP] = previousTask.process.map(f)
+      override protected def process: RIO[R, OP] = previousTask.process.map(fn)
       override val name: String                  = previousTask.name
     }
 }
--- modules/core/src/test/scala/etlflow/PipelineTestSuite.scala
+++ modules/core/src/test/scala/etlflow/PipelineTestSuite.scala
@@ -11,6 +11,16 @@ import zio.test._
 object PipelineTestSuite extends ApplicationLogger {
   def processData(msg: String): Unit = logger.info(s"Hello World from $msg")
 
+  def sendString(msg: String): String = {
+    logger.info(s"Sent $msg")
+    msg
+  }
+
+  def sendInt(msg: Int): Int = {
+    logger.info(s"Sent $msg")
+    msg
+  }
+
   val task1: GenericTask[Unit] = GenericTask(
     name = "ProcessData1",
     function = processData("ProcessData1")
@@ -21,35 +31,35 @@ object PipelineTestSuite extends ApplicationLogger {
     function = processData("ProcessData2")
   )
 
-  val task3: GenericTask[Unit] = GenericTask(
+  val task3: GenericTask[String] = GenericTask(
     name = "ProcessData3",
-    function = processData("ProcessData3")
+    function = sendString("ProcessData3")
   )
 
-  val task4: GenericTask[Unit] = GenericTask(
+  val task4: GenericTask[Int] = GenericTask(
     name = "ProcessData4",
-    function = processData("ProcessData4")
+    function = sendInt(100)
   )
 
   val spec: Spec[Audit, Any] =
     suite("Generic Task")(
       test("Test GenericETLTask pipeline") {
-        logger.info(s"EtlTask.RT ${EtlTask.RT.get()}")
-        val pipeline: EtlTask[Any, Unit] = task1 *> task2 *> task3 *> task4
-        logger.info(s"EtlTask.RT ${EtlTask.RT.get()}")
+        logger.info(s"EtlTask.taskSet ${EtlTask.taskSet.get()}")
+        val pipeline = task1 *> task2 *> task3 *> task4
+        logger.info(s"EtlTask.taskSet ${EtlTask.taskSet.get()}")
         logger.info(s"Task Name ${pipeline.name}")
         val pipelineZIO = pipeline.execute.provide(audit.console)
         assertZIO(pipelineZIO.foldZIO(ex => ZIO.succeed(ex.getMessage), _ => ZIO.succeed("ok")))(equalTo("ok"))
       },
       test("Test GenericETLTask for pipeline") {
-        logger.info(s"EtlTask.RT ${EtlTask.RT.get()}")
+        logger.info(s"EtlTask.taskSet ${EtlTask.taskSet.get()}")
         val pipeline = for {
-          _ <- task1
-          _ <- task2
-          _ <- task3
-          _ <- task4
-        } yield ()
-        logger.info(s"EtlTask.RT ${EtlTask.RT.get()}")
+          _   <- task1
+          _   <- task2
+          _   <- task3
+          op4 <- task4
+        } yield op4
+        logger.info(s"EtlTask.taskSet ${EtlTask.taskSet.get()}")
         logger.info(s"Task Name ${pipeline.name}")
         val pipelineZIO = pipeline.execute.provide(audit.console)
         assertZIO(pipelineZIO.foldZIO(ex => ZIO.succeed(ex.getMessage), _ => ZIO.succeed("ok")))(equalTo("ok"))

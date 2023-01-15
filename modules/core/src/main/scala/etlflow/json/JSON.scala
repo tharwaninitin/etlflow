cdbe52e60767e06016b74d8e6a623bed3ce7ae0c
Nitin Tharwani <tharwaninitin182@gmail.com>
2023-01-15 14:34:19 +0530

    Updated json module

---
+++ modules/core/src/main/scala/etlflow/json/JSON.scala
@@ -0,0 +1,30 @@
+package etlflow.json
+
+import etlflow.model.EtlFlowException.JsonDecodeException
+import zio._
+import zio.json._
+
+trait JSON {
+  def convertToObject[T](str: String)(implicit decoder: JsonDecoder[T]): IO[JsonDecodeException, T]
+  def convertToString[A](obj: A)(implicit encoder: JsonEncoder[A]): UIO[String]
+}
+
+object JSON {
+  def convertToObjectM[T](str: String)(implicit decoder: JsonDecoder[T]): ZIO[JSON, JsonDecodeException, T] =
+    ZIO.environmentWithZIO(_.get.convertToObject[T](str))
+  def convertToStringM[T](obj: T)(implicit encoder: JsonEncoder[T]): URIO[JSON, String] =
+    ZIO.environmentWithZIO(_.get.convertToString[T](obj))
+
+  def convertToObject[T](str: String)(implicit decoder: JsonDecoder[T]): Either[String, T] = str.fromJson[T]
+
+  def convertToString[T](obj: T)(implicit decoder: JsonEncoder[T]): String       = obj.toJson
+  def convertToStringPretty[T](obj: T)(implicit decoder: JsonEncoder[T]): String = obj.toJsonPretty
+
+  lazy val live: ULayer[JSON] = ZLayer.succeed(
+    new JSON {
+      override def convertToObject[T](str: String)(implicit decoder: JsonDecoder[T]): IO[JsonDecodeException, T] =
+        ZIO.fromEither(str.fromJson[T]).mapError(str => JsonDecodeException(str))
+      override def convertToString[T](obj: T)(implicit encoder: JsonEncoder[T]): UIO[String] = ZIO.succeed(obj.toJson)
+    }
+  )
+}

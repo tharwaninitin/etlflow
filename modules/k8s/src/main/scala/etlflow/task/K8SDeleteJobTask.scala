8c5a63fb12b84c9fb6d6e4e782f3a68963f39838
Hari Krishnan <rite2hari347@gmail.com>
2023-01-21 17:24:55 +0530

    Added Config Descriptor for K8SDeleteJobTask

--- modules/k8s/src/main/scala/etlflow/task/K8SDeleteJobTask.scala
+++ modules/k8s/src/main/scala/etlflow/task/K8SDeleteJobTask.scala
@@ -1,6 +1,8 @@
 package etlflow.task
 
 import etlflow.k8s._
+import zio.config._
+import ConfigDescriptor._
 import zio.{RIO, ZIO}
 
 /** Deletes the Job after specified time
@@ -20,27 +22,37 @@ import zio.{RIO, ZIO}
 case class K8SDeleteJobTask(
     name: String,
     jobName: String,
-    namespace: String = "default",
-    gracePeriodInSeconds: Int = 0,
-    debug: Boolean = false
+    namespace: Option[String] = None,
+    gracePeriodInSeconds: Option[Int] = None,
+    debug: Option[Boolean] = None
 ) extends EtlTask[K8S, Unit] {
 
   override def getTaskProperties: Map[String, String] = Map(
     "name"                   -> name,
     "jobName"                -> jobName,
-    "namespace"              -> namespace,
-    "debug"                  -> debug.toString,
-    "deletionGraceInSeconds" -> gracePeriodInSeconds.toString
+    "namespace"              -> namespace.getOrElse("default"),
+    "debug"                  -> debug.getOrElse(false).toString,
+    "deletionGraceInSeconds" -> gracePeriodInSeconds.getOrElse(0).toString
   )
 
   override protected def process: RIO[K8S, Unit] = for {
     _ <- ZIO.logInfo("#" * 50)
     _ <- ZIO.logInfo(s"Deleting Job $jobName")
     _ <- K8S
-      .deleteJob(jobName, namespace, gracePeriodInSeconds, debug)
+      .deleteJob(jobName, namespace.getOrElse("default"), gracePeriodInSeconds.getOrElse(0), debug.getOrElse(false))
       .tapBoth(
         ex => ZIO.logError(ex.getMessage),
         _ => ZIO.logInfo(s"Deleted Job $jobName") *> ZIO.logInfo("#" * 50)
       )
   } yield ()
+}
+
+object K8SDeleteJobTask {
+  val config: ConfigDescriptor[K8SDeleteJobTask] =
+    string("name")
+      .zip(string("jobName"))
+      .zip(string("namespace").optional)
+      .zip(int("gracePeriodInSeconds").optional)
+      .zip(boolean("debug").optional)
+      .to[K8SDeleteJobTask]
 }

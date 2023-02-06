bd553ca22e3e8c5200fda74fc08ac06c7f3f7bc5
Nitin Tharwani <tharwaninitin182@gmail.com>
2023-02-06 23:37:53 +0530

    Update DBImpl.scala

--- modules/jdbc/src/main/scala/etlflow/db/DBImpl.scala
+++ modules/jdbc/src/main/scala/etlflow/db/DBImpl.scala
@@ -1,40 +1,28 @@
 package etlflow.db
 
 import etlflow.log.ApplicationLogger
-import etlflow.model.EtlFlowException.DBException
 import scalikejdbc.{NamedDB, NoExtractor, SQL, WrappedResultSet}
 import zio._
 
 private[etlflow] case class DBImpl(poolName: String, fetchSize: Option[Int]) extends DB with ApplicationLogger {
-  override def executeQuery(query: String): IO[DBException, Unit] =
-    ZIO
-      .attempt(NamedDB(poolName).localTx(implicit s => scalikejdbc.SQL(query).update()))
-      .mapError { e =>
-        logger.error(e.getMessage)
-        DBException(e.getMessage)
-      }
-      .unit
-  override def executeQuery(query: SQL[Nothing, NoExtractor]): IO[DBException, Unit] =
-    ZIO
-      .attempt(NamedDB(poolName).localTx(implicit s => query.update()))
-      .mapError { e =>
-        logger.error(e.getMessage)
-        DBException(e.getMessage)
-      }
-      .unit
+
+  override def executeQuery(query: String): Task[Unit] = ZIO
+    .attempt(NamedDB(poolName).localTx(implicit s => scalikejdbc.SQL(query).update()))
+    .tapError(e => ZIO.logError(e.getMessage))
+    .unit
+
+  override def executeQuery(query: SQL[Nothing, NoExtractor]): Task[Unit] = ZIO
+    .attempt(NamedDB(poolName).localTx(implicit s => query.update()))
+    .tapError(e => ZIO.logError(e.getMessage))
+    .unit
+
   @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
-  override def fetchResult[T](query: String)(fn: WrappedResultSet => T): IO[DBException, T] =
-    ZIO
-      .attempt(NamedDB(poolName).localTx(implicit s => scalikejdbc.SQL(query).map(fn).single().get))
-      .mapError { e =>
-        logger.error(e.getMessage)
-        DBException(e.getMessage)
-      }
-  override def fetchResults[T](query: String)(fn: WrappedResultSet => T): IO[DBException, Iterable[T]] =
-    ZIO
-      .attempt(NamedDB(poolName).localTx(implicit s => scalikejdbc.SQL(query).fetchSize(fetchSize).map(fn).iterable()))
-      .mapError { e =>
-        logger.error(e.getMessage)
-        DBException(e.getMessage)
-      }
+  override def fetchResult[T](query: String)(fn: WrappedResultSet => T): Task[T] = ZIO
+    .attempt(NamedDB(poolName).localTx(implicit s => scalikejdbc.SQL(query).map(fn).single().get))
+    .tapError(e => ZIO.logError(e.getMessage))
+
+  @SuppressWarnings(Array("org.wartremover.warts.ToString"))
+  override def fetchResults[T](query: String)(fn: WrappedResultSet => T): Task[Iterable[T]] = ZIO
+    .attempt(NamedDB(poolName).localTx(implicit s => scalikejdbc.SQL(query).fetchSize(fetchSize).map(fn).iterable()))
+    .tapError(e => ZIO.logError(e.getMessage))
 }
